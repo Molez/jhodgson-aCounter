4170f2d0b92a5fd63fd01997a48dae1e9652f2bd
Molez <hodgson.james.r@gmail.com>
2014-01-11 13:15:11 -0700

    Included functionality for dynamic list generation
    Currently only work for strings, next step is to turn the strings into
    counter objects.

--- src/com/example/jhodgson_acounter/MainActivity.java
+++ src/com/example/jhodgson_acounter/MainActivity.java
@@ -6,28 +6,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+//author Java Experience; https://www.youtube.com/watch?v=tNoeFkXCZ6w
+//The list functionality was adapted from Java Experience
 public class MainActivity extends Activity {
 	
+	public final static String EXTRA_MESSAGE = "com.example.jhodgson-acounter.MESSAGE";
 	private ArrayList<String> strings = new ArrayList<String>();
 	private ArrayAdapter<String> adapter;
 	private ListView countersListView;
+	private Button button;
+	private EditText counterName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         countersListView = (ListView) findViewById(R.id.listView1);
+        button = (Button) findViewById(R.id.new_counter);
+        counterName = (EditText) findViewById(R.id.counter_Name);
         
         //Setting up the counters list
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
         countersListView.setAdapter(adapter);
+        button.setOnClickListener(new OnClickListener() {
+        	
+        	//On Click functionality
+        	@Override
+        	public void onClick(View arg0){
+        		strings.add(counterName.getText().toString());
+        		counterName.setText(""); //Clear the text after use
+        		adapter.notifyDataSetChanged();
+        	}
+        }); 
         
-        for(int i=0; i<50; i++){
-        	strings.add("test" + i);
-        }
     }
 
     @Override
@@ -36,10 +54,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    /** Called when the user clicks the New button */
-    public void newCounter(View view) {
-        // Do something in response to button
-    }
-    
 }
